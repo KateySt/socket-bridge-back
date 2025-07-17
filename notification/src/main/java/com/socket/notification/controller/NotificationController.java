b828e66a830c37cbfb1c5ba7743312ff3a32ff85
KateSt <89999131+KateySt@users.noreply.github.com>
2025-07-17 06:54:27 +0300

    Update NotificationController.java

--- notification/src/main/java/com/socket/notification/controller/NotificationController.java
+++ notification/src/main/java/com/socket/notification/controller/NotificationController.java
@@ -3,7 +3,6 @@ package com.socket.notification.controller;
 import com.socket.notification.dto.NotificationRequest;
 import com.socket.notification.service.NotificationService;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,7 +11,6 @@ import reactor.core.publisher.Mono;
 
 @RestController
 @RequestMapping("/api/notifications")
-@Slf4j
 @RequiredArgsConstructor
 public class NotificationController {
 
@@ -20,7 +18,6 @@ public class NotificationController {
 
     @PostMapping
     public Mono<Void> create(@RequestBody NotificationRequest request) {
-        System.out.println(request.toString());
         service.saveNotification(request.getUserId(), request.getText());
         return Mono.empty();
     }
